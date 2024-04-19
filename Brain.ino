c581340e958ec3438c4a57be2b6de947372ff3b1
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-04-19 00:32:52 -0300

    Updated to latest

--- Brain.ino
+++ Brain.ino
@@ -1,63 +1,75 @@
 #include <Arduino.h>
 #include "Serial/package.h"
 
-
-uint8_t g_c = 0;
+bool must_check_again = false;
 
 void setup()
 {
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(115200);
+  while(!Serial);
 
-	MMSerial::setup(MMSerial::device_id::MASTER);
+  CustomSerial::begin_master();
+  //CustomSerial::check_devices_online_if_not_checked();
 
-  Serial.println("Begin!");
+  attachInterrupt(digitalPinToInterrupt(0), []{
+    Serial.printf("User requested check. Set to check.\n");
+    must_check_again = true;//CustomSerial::check_all_devices_online();
+  }, RISING);
+  
+  // add button to check via attach using set_pin_to... later
 }
 
-
-
 void loop()
-{  
-	auto v = MMSerial::read_data();
-  if (!v) {
-    switch(g_c = (g_c + 1) % 5) {
-    case 0: Serial.println("No data in serial yet"); break;
-    case 1: Serial.println("No data in serial yet."); break;
-    case 2: Serial.println("No data in serial yet.."); break;
-    case 3: Serial.println("No data in serial yet..."); break;
-    default: Serial.println("No data in serial yet...."); break;
-    }    
-    delay(1000);
+{
+  if (!CustomSerial::is_any_device_connected() || must_check_again) {
+    must_check_again = false;
+    Serial.printf("Re-checking devices...\n");
+    CustomSerial::check_all_devices_online();
+    delay(5000);
+    return;
   }
-  else {    
-		switch (v->get_type()) {
-		case MMSerial::data_type::T_F:
-			Serial.print("Got float: ");
-      Serial.print(v->get_path());
-      Serial.print(" -> ");
-      Serial.println(v->get_as_float());
-			break;
-		case MMSerial::data_type::T_D:
-			Serial.print("Got double: ");
-      Serial.print(v->get_path());
-      Serial.print(" -> ");
-      Serial.println(v->get_as_double());
-			break;
-		case MMSerial::data_type::T_I:
-			Serial.print("Got integer: ");
-      Serial.print(v->get_path());
-      Serial.print(" -> ");
-      Serial.println(v->get_as_int());
-			break;
-		case MMSerial::data_type::T_U:
-			Serial.print("Got unsigned: ");
-      Serial.print(v->get_path());
-      Serial.print(" -> ");
-      Serial.println(v->get_as_uint());
-			break;
-		default:
-			Serial.println("Unknown error");
-		}
+
+  for (uint8_t id = 0; id < CustomSerial::get_devices_limit(); ++id) {
+    if (!CustomSerial::is_device_connected(id)) {
+      //Serial.printf("Skipped id %d (offline / unconfigured)...\n", id);
+      continue;
+    }
+
+    Serial.printf("Requesting id %d...\n", id);
+    CustomSerial::request(id);
     delay(100);
-  }
+
+    //Serial.printf("Wire has %d bytes to read.\n", Wire.available());
+
+    CustomSerial::command_package cmds;
+    CustomSerial::read(cmds);
+    
+    Serial.printf("Received %u infos.\n", static_cast<unsigned>(cmds.size()));
+
+    for(size_t p = 0; p < cmds.size(); ++p) {
+      const auto& cmd = cmds.idx(p);
+
+      Serial.printf("Data came from %hu @ path %s: ",
+          (uint16_t)cmd.get_device_id(), cmd.get_path());
+
+      switch(cmd.get_val_type()) {
+      case CustomSerial::command::vtype::TD:
+        Serial.print(cmd.get_val().d);
+        break;
+      case CustomSerial::command::vtype::TF:
+        Serial.print(cmd.get_val().f);
+        break;
+      case CustomSerial::command::vtype::TI:
+        Serial.print(cmd.get_val().i);
+        break;
+      case CustomSerial::command::vtype::TU:
+        Serial.print(cmd.get_val().u);
+        break;
+      default:
+        Serial.print("Unhandled.");
+      }
+      Serial.println();
+      //delay(250);
+    }
+  }  
 }
