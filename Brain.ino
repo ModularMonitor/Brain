09dcd3cd5e640862fd55c1cb4b4f59d55c650c36
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-04-29 18:58:25 -0300

    [@Lohkdesgds] Updated Serial and followed new format

--- Brain.ino
+++ Brain.ino
@@ -1,79 +1,66 @@
-#include <Arduino.h>
-#include "Serial/package.h"
+//#define _DEBUG
+#include "Serial/packaging.h"
 
-bool must_check_again = true;
+using namespace CS;
+
+PackagedWired* wire;
 constexpr int led_pin = 2;
+bool must_check_again = true;
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Waiting 5 sec to boot");
-    delay(5000);
-    while(!Serial);
-    pinMode(led_pin, OUTPUT);
+    while(!Serial || Serial.available() == 0);
     
+    pinMode(led_pin, OUTPUT);
     digitalWrite(led_pin, HIGH);
     
-    CustomSerial::set_logging(Serial);
-    CustomSerial::print_info();
-    CustomSerial::begin_master();
-    
-    digitalWrite(led_pin, LOW);
+    Serial.printf("Starting MASTER\n");
+    wire = new PackagedWired(config().set_master());    
     
     attachInterrupt(digitalPinToInterrupt(0), []{
         Serial.printf("User requested check. Set to check.\n");
         digitalWrite(led_pin, HIGH);    
         must_check_again = true;
     }, RISING);
+    
+    digitalWrite(led_pin, LOW);
 }
 
 void loop()
 {
-    if (!CustomSerial::is_any_device_connected() || must_check_again) {
-        digitalWrite(led_pin, HIGH);
-        if (must_check_again) Serial.printf("Waiting for devices...\n");
-        must_check_again = false;
-        CustomSerial::check_all_devices_online();
-        delay(4900);
-        digitalWrite(led_pin, LOW);
-        delay(100);
-        return;
-    }
-
-  for (uint8_t id = 0; id < CustomSerial::get_devices_limit(); ++id) {
-        if (!CustomSerial::is_device_connected(id)) continue;
-    
-        Serial.printf("%d ...", (int)id);
-        delay(500);
+    Serial.printf("### Beginning new requests... ###\n");
+    for(uint8_t p = 0; p < d2u(device_id::_MAX); ++p) {
+        const device_id curr = static_cast<device_id>(p);
+        const char* device_name = d2str(curr);
         
-        CustomSerial::request(id);
-        CustomSerial::command_package cmds;
-        CustomSerial::read(cmds);
+        //Serial.printf("Requesting %s... ", d2str(curr));
         
-        Serial.printf("! got %u infos:\n", static_cast<unsigned>(cmds.size()));
+        const auto lst = wire->master_request_all(curr);
         
-    
-        for(size_t p = 0; p < cmds.size(); ++p) {
-            delay(500);
-            const auto& cmd = cmds.idx(p);
-        
-            Serial.printf("- %hu @ %s => ", (uint16_t)cmd.get_device_id(), cmd.get_path());
-        
-            switch(cmd.get_val_type()) {
-            case CustomSerial::command::vtype::TF:
-                Serial.print(cmd.get_val().f);
+        for(const auto& i : lst) {
+            Serial.printf("> %s: %s = ", device_name, i.get_path());
+            switch(i.get_type()) {
+            case Command::vtype::TD:
+                Serial.print(i.get_val<double>());
                 break;
-            case CustomSerial::command::vtype::TI:
-                Serial.print(cmd.get_val().i);
+            case Command::vtype::TF:
+                Serial.print(i.get_val<float>());
                 break;
-            case CustomSerial::command::vtype::TU:
-                Serial.print(cmd.get_val().u);
+            case Command::vtype::TI:
+                Serial.print(i.get_val<int64_t>());
+                break;
+            case Command::vtype::TU:
+                Serial.print(i.get_val<uint64_t>());
                 break;
             default:
-                Serial.print("Unhandled/error");
+                Serial.print("Unknown");
+                break;
             }
             Serial.println();
         }
-    }  
-    delay(500);
+    }
+    Serial.printf("### Ended row of requests ###\n");
+    
+    delay(5000);
 }
